b59d6822febd4932b9fdeadb6fc84d7cbdeb57b8
srac0005 <srac0005@student.monash.edu>
2023-12-18 20:38:57 +0100

    chore(api/route): move /use/me endpoint definition

--- api/routes/api.php
+++ api/routes/api.php
@@ -19,12 +19,7 @@ use App\Http\Controllers\API\EventController;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user/me', function (Request $request) {
-    return $request->user();
-});
-
-Route::controller(RegisterController::class)->group(function(){
+Route::controller(RegisterController::class)->group(function () {
     Route::post('login', 'login');
 
     Route::post('logout', 'logout')
@@ -35,7 +30,10 @@ Route::controller(RegisterController::class)->group(function(){
         ->middleware('admin');
 });
 
-Route::middleware('auth:sanctum')->group( function () {
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user/me', function (Request $request) {
+        return $request->user();
+    });
     Route::get('users', [UsersController::class, 'index']);
     Route::get('users/{user}', [UsersController::class, 'show']);
 
@@ -45,7 +43,7 @@ Route::middleware('auth:sanctum')->group( function () {
     Route::get('event', [EventController::class, 'index']);
     Route::get('event/{uuid}', [EventController::class, 'show']);
 
-    Route::middleware('admin')->group( function () {
+    Route::middleware('admin')->group(function () {
         // Admin access only
         Route::put('users/{user}', [UsersController::class, 'update']);
         Route::delete('users/{user}', [UsersController::class, 'destroy']);
@@ -63,3 +61,4 @@ Route::middleware('auth:sanctum')->group( function () {
         Route::delete('event/{uuid}', [EventController::class, 'destroy']);
     });
 });
+
