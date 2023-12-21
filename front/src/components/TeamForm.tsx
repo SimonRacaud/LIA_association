92106c7c89611494256a9e60b119bb02a4c404a0
srac0005 <srac0005@student.monash.edu>
2023-12-21 16:38:00 +0100

    fix(front): remove member in event from teams edit

--- front/src/components/TeamForm.tsx
+++ front/src/components/TeamForm.tsx
@@ -1,8 +1,13 @@
-import { Card, Stack, Typography } from "@mui/material";
+import { Box, Card, Stack, Typography } from "@mui/material";
 import TeamTemplateForm from "./TeamTemplateForm";
 import Team from "classes/Team";
 import TeamTemplate from "classes/TeamTemplate";
 import UserTable from "./UserTable";
+import TeamService from "services/TeamService";
+import { AxiosError } from "axios";
+import NetErrorBody, { NetFailureBody } from "models/ErrorResponse";
+import { useState } from "react";
+import ErrorNotification from "./ErrorNotification";
 
 type TeamFormProps = {
     team: Team
@@ -11,28 +16,49 @@ type TeamFormProps = {
 
 export default function TeamForm({ team, setTeam }: TeamFormProps)
 {
+    const [ errorNet, setErrorNet ] = useState<NetErrorBody>()
+
     const updateTeamTemplate = (template: TeamTemplate, update: boolean) => {
         team.template = template
         setTeam(team, update)
         // TODO : API CALL ?
     }
-    const onRemoveUser = (uuid: string) => {
-        team.members = team.members.filter((user) => user.id != uuid)
-        setTeam(team, true)
-        // TODO : API CALL
+    const onRemoveUser = async (uuid: string) => {
+        try {
+            // API Update
+            await TeamService.memberUnsubscribe(team.uuid, uuid)
+            // Remove 'user" from team
+            team.members = team.members.filter((user) => user.id != uuid)
+            setTeam(team, true)
+        } catch(error) {
+            const errorNetwork = error as AxiosError
+            const errorBody = errorNetwork.response?.data as NetErrorBody
+            console.error(errorNetwork.message)
+            if (errorBody) {
+                setErrorNet(NetFailureBody)
+            } else {
+                setErrorNet(errorBody)
+            }
+        }
     }
 
     return (
-        <Stack spacing={2}>
-            {/* Template */}
-            <TeamTemplateForm template={team.template} setTemplate={updateTeamTemplate} lock={true} />
-            {/* Members */}
-            {team.members.length > 0 &&
-            <Card sx={{ m: 1, p: 1 }}>
-                <Typography sx={{ my: 1 }}>Membres inscrits:</Typography>
-                <UserTable userList={team.members} onRemoveUser={onRemoveUser} />
-            </Card>
-            }
-        </Stack>
+        <Box>
+            <Stack spacing={2}>
+                {/* Template */}
+                <TeamTemplateForm template={team.template} setTemplate={updateTeamTemplate} lock={true} />
+                {/* Members */}
+                {team.members.length > 0 &&
+                <Card sx={{ m: 1, p: 1 }}>
+                    <Typography sx={{ my: 1 }}>Membres inscrits:</Typography>
+                    <UserTable userList={team.members} onRemoveUser={onRemoveUser} />
+                </Card>
+                }
+            </Stack>
+            <ErrorNotification show={errorNet != undefined}
+                onClose={() => setErrorNet(undefined)}
+                netError={errorNet}
+            />
+        </Box>
     )
 }
