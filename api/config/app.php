202dd3ab97125616ef709aa89118aa65e2574b29
srac0005 <srac0005@student.monash.edu>
2023-12-21 13:53:11 +0100

    chore(api): update readme. update .env.example file

--- api/config/app.php
+++ api/config/app.php
@@ -16,7 +16,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'LIA_planning_api'),
 
     /*
     |--------------------------------------------------------------------------
