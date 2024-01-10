99bf622e60965b17e853e0e569be8f203993d6f6
srac0005 <srac0005@student.monash.edu>
2024-01-10 15:01:34 +0100

    refactor(api): in database seeder, add default 'distrib' team templates

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -28,41 +28,60 @@ class DatabaseSeeder extends Seeder
             dump("Seeder: admin user not created.", $e->getMessage());
         }
         try {
-            TeamTemplate::create([
+            TeamTemplate::updateOrCreate([
                 "title" => "Leclerc sablé / Super U Arnage",
                 "type" => TeamType::RAMASSAGE,
                 "note" => "9876 A",
                 "maxMember" => 2
             ]);
-            TeamTemplate::create([
+            TeamTemplate::updateOrCreate([
                 "title" => "Carrefour Sud / La Pointe",
                 "type" => TeamType::RAMASSAGE,
                 "note" => "02.43.67.30.96 / 2312 (V)",
                 "maxMember" => 2
             ]);
-            TeamTemplate::create([
+            TeamTemplate::updateOrCreate([
                 "title" => "U express bollée",
                 "type" => TeamType::RAMASSAGE,
                 "note" => "02.43.84.57.61",
                 "maxMember" => 1
             ]);
-            TeamTemplate::create([
+            TeamTemplate::updateOrCreate([
                 "title" => "Leclerc Fontenelle Drive / Super U Bonnétable",
                 "type" => TeamType::RAMASSAGE,
                 "note" => "1234 / 2312 (V)",
                 "maxMember" => 2
             ]);
-            TeamTemplate::create([
+            TeamTemplate::updateOrCreate([
                 "title" => "Utile / St George du bois",
                 "type" => TeamType::RAMASSAGE,
                 "note" => "2312 (v)",
                 "maxMember" => 1
             ]);
-            TeamTemplate::create([
-                "title" => "Distribution",
+            // Distribution
+            TeamTemplate::updateOrCreate([
+                "title" => "Accueil / Pain",
                 "type" => TeamType::DISTRIBUTION,
                 "note" => "10h - 14h, au local",
-                "maxMember" => 5
+                "maxMember" => 1
+            ]);
+            TeamTemplate::updateOrCreate([
+                "title" => "Frais / Viennoiseries",
+                "type" => TeamType::DISTRIBUTION,
+                "note" => "10h - 14h, au local",
+                "maxMember" => 2
+            ]);
+            TeamTemplate::updateOrCreate([
+                "title" => "Fruits",
+                "type" => TeamType::DISTRIBUTION,
+                "note" => "10h - 14h, au local",
+                "maxMember" => 1
+            ]);
+            TeamTemplate::updateOrCreate([
+                "title" => "Légumes",
+                "type" => TeamType::DISTRIBUTION,
+                "note" => "10h - 14h, au local",
+                "maxMember" => 2
             ]);
         } catch (QueryException $e) {
             dump("Seeded: templates not created", $e->getMessage());
