127058035c46b2583b179d0b831884bbb45b0b35
srac0005 <srac0005@student.monash.edu>
2024-01-21 12:14:13 +0100

    chore(front): in error response enum, translate the error labels in french

--- front/src/models/ErrorResponse.ts
+++ front/src/models/ErrorResponse.ts
@@ -1,10 +1,10 @@
 
 export enum ErrorType {
-    VALIDATION = 'Validation Error',
-    BODY = 'Body Error',
-    FAILURE = 'Failure',
-    NOT_FOUND = 'Not found',
-    DENIED = 'Access denied'
+    VALIDATION = 'Erreur de validation',
+    BODY = 'Erreur de contenu',
+    FAILURE = 'Critique',
+    NOT_FOUND = 'Introuvable',
+    DENIED = 'Accès interdit'
 }
 
 export const NetFailureBody =  { message: ErrorType.FAILURE, data: "Erreur réseau" }
